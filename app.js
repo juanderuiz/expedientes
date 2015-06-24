f318f334dc078d7bd9429619d81ee26f809fe9f4
juanderuiz <juanderuiz@gmail.com>
2015-06-24 17:57:14 +0200

    Index.html with list of expedientes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+var expedientes = ["01001/15", "01002/15", "12003/15"];
+
 app.get('/', function(req,res){
   res.send('OK');
+});
+
+app.get('/expedientes', function(req,res){
+  //res.send('OK');
+  res.json(expedientes);
 });
 
 //app.listen(3000);
--- test.js
+++ test.js
@@ -7,11 +7,51 @@ describe('Request to the root path', function(){
 
   	request(app)
     .get('/')
-    .expect(200)
-    .end(function(error){
-    	if (error) throw error;
-    	done();
-    });
+    .expect(200, done);
+
+  });
+
+  it('Returns HTML format', function(done){
+
+    request(app)
+    .get('/')
+    .expect('Content-Type', /html/, done);
+
+  });
+
+  it ('Returns an index file with the word Expedientes in the content', function(done){
+   
+    request(app)
+    .get('/')
+    .expect(/expedientes/i, done);
+
+  });
+  
+});
+
+describe('List all expedientes in /expedientes', function(){
+
+  it('Returns a 200 status code', function(done) {
+
+  	request(app)
+    .get('/expedientes')
+    .expect(200, done);
+
+  });
+
+  it('Returns JSON', function(done) {
+
+  	request(app)
+    .get('/expedientes')
+    .expect('Content-Type', /json/, done);
+
+  });
+
+  it('Returns all expedientes', function(done) {
+
+  	request(app)
+    .get('/expedientes')
+    .expect(JSON.stringify(["01001/15", "01002/15", "12003/15"]), done);
 
   });
   
